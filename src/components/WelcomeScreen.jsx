67b9abcc1210af2c2ba50da536cb138728b8361e
Kmkim <akkn920@naver.com>
2025-08-06 04:30:26 +0900

    0806 04:30

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -3,9 +3,9 @@ import { Info, MapPin, Calendar, ClipboardList } from 'lucide-react';
 
 const icons = {
   "소개": <Info size={28} className="menu-icon" />,
-  "오시는 길 & 연락처": <MapPin size={28} className="menu-icon" />,
+  "오시는 길 ": <MapPin size={28} className="menu-icon" />,
   "투어 안내": <Calendar size={28} className="menu-icon" />,
-  "아카데미 모집": <ClipboardList size={28} className="menu-icon" />,
+  "아카데미": <ClipboardList size={28} className="menu-icon" />,
 };
 
 const WelcomeScreen = ({ pages, onSelectPage }) => {
@@ -17,12 +17,17 @@ const WelcomeScreen = ({ pages, onSelectPage }) => {
         궁금한 내용을 아래 버튼으로 선택해 보세요.
       </div>
       <div className="menu-grid">
-        {Object.entries(pages).map(([key]) => (
-          <button key={key} onClick={() => onSelectPage(key)} className="menu-button">
-            {icons[key]}
-            <span className="menu-label">{key}</span>
-          </button>
-        ))}
+      {Object.keys(icons).map((key) => (
+        <button
+          key={key}
+          onClick={() => onSelectPage(key)}
+          className="menu-button"
+        >
+          {icons[key]}
+          <span className="menu-label">{key}</span>
+        </button>
+      ))}
+
       </div>
     </div>
   );
